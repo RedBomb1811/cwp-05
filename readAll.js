f37c4f393494f20e1a0af988aed1144f1e5aec4e
RedBomb1811 <dimadudko00@gmail.com>
2018-10-26 22:50:45 +0300

    readall 4 ex

--- readAll.js
+++ readAll.js
@@ -8,6 +8,10 @@ function readAll(req, res, payload, cb) {
         payload.sortField = 'date';
     if(payload.sortOrder === undefined)
         payload.sortOrder = 'desc';
+    if(payload.page === undefined)
+        payload.page = 1;
+    if(payload.limit === undefined)
+        payload.limit = 10;
 
     articles.articles.sort((a, b)=>{
         if((a[payload.sortField] > b[payload.sortField]) ^ (payload.sortOrder === 'asc'))
@@ -16,6 +20,16 @@ function readAll(req, res, payload, cb) {
             return 1;
         else return 0;
     });
-    cb(null, articles.articles)
+
+    let begin = (payload.page - 1)*payload.limit;
+    let end = begin + payload.limit;
+    let reqArr = {};
+    reqArr.items = articles.articles.slice(begin, end);
+    reqArr.page = payload.page;
+    reqArr.pages = Math.ceil(articles.articles.length / payload.limit);
+    reqArr.count = articles.articles.length;
+    reqArr.limit = payload.limit;
+
+    cb(null, reqArr);
 }
 exports.readAll = readAll;
